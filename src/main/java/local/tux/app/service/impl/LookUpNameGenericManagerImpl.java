56d1b5fdd0eaa45d252f18f59b38d74ad5ac3392
benli3060 <benli3060@7a33551c-9a6b-11de-9965-3f0b11712531>
2009-10-15 06:42:08 +0000

    merge some of the generic to trunk

---
+++ src/main/java/local/tux/app/service/impl/LookUpNameGenericManagerImpl.java
@@ -0,0 +1,57 @@
+package local.tux.app.service.impl;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+import local.tux.app.Constants;
+import local.tux.app.dao.TuxNameGenericDao;
+import local.tux.app.model.common.TuxBaseObject;
+import local.tux.app.service.LookUpNameGenericManager;
+
+import org.appfuse.dao.GenericDao;
+import org.appfuse.service.impl.GenericManagerImpl;
+
+public class LookUpNameGenericManagerImpl<T, PK extends Serializable> extends GenericManagerImpl<T, PK> implements
+		LookUpNameGenericManager<T, PK> {
+
+	public TuxNameGenericDao<T, PK> genericDao ;
+	@SuppressWarnings("unchecked")
+	public LookUpNameGenericManagerImpl(GenericDao<T, Long> genericDao) {
+		super((GenericDao<T, PK>) genericDao);
+		this.genericDao = (TuxNameGenericDao) genericDao;
+		
+	}
+
+	public T getLookUpBaseObject(String name) {
+		return (T) genericDao.getLookUpBaseObject(name);
+	}
+
+	public List<T> findByNamedQuery(String queryName,
+			Map<String, Object> queryParams) {
+		
+		return  genericDao.findByNamedQuery(queryName, queryParams);
+		
+	}
+	public List<T> getAllDistinct() {
+		
+		return genericDao.getAllDistinct();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<TuxBaseObject> getRelativeObjects(String key, Long value) {
+		return genericDao.getRelativeObjects(key, value);
+	}
+	public List<T> search(String propertyName, String value){
+		
+		return genericDao.search(propertyName ,value, Constants.AJAX_SIZE);
+		
+	}
+
+	public int getAllRecordsCount() {
+		return genericDao.getAllRecordsCount();
+	}
+
+}
---
+++ src/main/java/local/tux/app/web/controller/TuxBaseObjectsController.java
@@ -0,0 +1,110 @@
+package local.tux.app.web.controller;
+
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import local.tux.app.Constants;
+import local.tux.app.model.common.TuxBaseObject;
+import local.tux.app.service.PagingLookupManager;
+import local.tux.app.web.paginated.list.ExtendedPaginatedList;
+import local.tux.app.web.paginated.list.PaginateListFactory;
+
+import org.apache.commons.lang.StringUtils;
+import org.appfuse.model.Role;
+import org.appfuse.model.User;
+import org.appfuse.service.UserManager;
+import org.displaytag.properties.SortOrderEnum;
+import org.hibernate.criterion.DetachedCriteria;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+ * general controller to handle display all request.
+ * @author Ben Li
+ *
+ */
+public class TuxBaseObjectsController implements Controller {
+
+	protected String KEY_REFERENCE_LIST = "tuxBaseObjectList";
+	protected PaginateListFactory factory;
+	protected  PagingLookupManager lookupManager;
+	protected Class<TuxBaseObject> clazz;
+	
+	protected final String HAS_PERMISSION = "hasPermission";
+	protected UserManager userManager;
+	
+	public void setUserManager(UserManager userManager){
+		this.userManager = userManager;
+	}
+	public void setPaginatedListFactory(PaginateListFactory factory){
+		this.factory = factory;
+	}
+	public void setPagingLookupManager(PagingLookupManager lookupManager){
+		this.lookupManager = lookupManager;
+	}
+	public void setPojo(Class<TuxBaseObject> clazz){
+		this.clazz = clazz;
+	}
+	public PaginateListFactory getPaginateListFactory() {
+        return factory;
+    }
+	
+	/**
+	 * the key for the default list is tuxBaseObjectList.
+	 * 
+	 */
+	public  ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws Exception{
+		ModelAndView mav = new ModelAndView();
+		String sortByColumn = request.getParameter("sort") == null ? "id" : request.getParameter("sort");
+		SortOrderEnum orderEnum = SortOrderEnum.DESCENDING;
+		String direction = request.getParameter("dir");
+		if ( direction != null && direction.contains("asc")){
+			orderEnum = SortOrderEnum.ASCENDING;
+		}
+		
+		if (StringUtils.isBlank(request.getRemoteUser())) {
+			mav.addObject(HAS_PERMISSION, Boolean.FALSE);
+		}else {
+			User user = userManager.getUserByUsername(request.getRemoteUser());
+			mav.addObject(HAS_PERMISSION, hasPermissionToAdd(user));
+		}
+		ExtendedPaginatedList paginatedList = factory.getPaginatedListFromRequest(request);
+		paginatedList.setPageSize(Constants.PAGE_SIZE);
+		paginatedList.setSortCriterion(sortByColumn);
+		paginatedList.setSortDirection(orderEnum);
+		lookupManager.getRecordsPage(getCriteria(request), paginatedList);
+		//((local.tux.app.model.Product)paginatedList.getList().get(0)).getImages().;
+		mav.addObject(KEY_REFERENCE_LIST,paginatedList);
+		return mav;
+	}
+	/**
+	 * you should override this function.
+	 * @return
+	 */
+	public DetachedCriteria getCriteria(HttpServletRequest request){
+		DetachedCriteria criteria = DetachedCriteria.forClass(clazz);
+		return criteria;
+		
+	}
+	protected Boolean hasPermissionToAdd(User user){
+		boolean result = Boolean.FALSE;
+		Set<Role> roles = user.getRoles();
+		for (Role role : roles){
+			for (String name : Constants.EDIT_PERMISSION){
+				if (name.equals(role.getName())){
+					result = Boolean.TRUE;
+					break;
+				}
+			}
+			if (result){
+				break;
+			}
+		}
+		return result;
+	}
+
+	
+}
