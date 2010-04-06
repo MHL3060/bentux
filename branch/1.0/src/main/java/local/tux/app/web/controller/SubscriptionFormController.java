8e67e8ed87389b9bf872a3103a8b879cc2548f8e
lhkhome <lhkhome@7a33551c-9a6b-11de-9965-3f0b11712531>
2010-04-06 05:47:43 +0000

    subscription

--- branch/1.0/src/main/java/local/tux/app/web/controller/SubscriptionFormController.java
+++ branch/1.0/src/main/java/local/tux/app/web/controller/SubscriptionFormController.java
@@ -1,12 +1,16 @@
 package local.tux.app.web.controller;
 
+import java.util.Date;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.appfuse.model.User;
+import org.appfuse.service.UserManager;
 import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,15 +18,19 @@ import com.octo.captcha.service.CaptchaService;
 
 import local.tux.Constants;
 import local.tux.app.model.Subscription;
-import local.tux.app.model.common.LookUpBaseObject;
 import local.tux.app.model.common.TuxBaseObject;
 import local.tux.app.web.common.controller.TuxBaseFormController;
 
 public class SubscriptionFormController extends TuxBaseFormController {
-
+	
+	private UserManager userManager;
 	private CaptchaService captchaService;
 
 	private Log log = LogFactory.getLog(SubscriptionFormController.class);
+	
+	public void setUserManager(UserManager userManager){
+		this.userManager = userManager;
+	}
 	
 	public void setCaptchaService(CaptchaService captchaService){
 		this.captchaService = captchaService;
@@ -36,6 +44,9 @@ public class SubscriptionFormController extends TuxBaseFormController {
 		boolean isNew = (baseObject.getId() == null);
 		Locale locale = request.getLocale();
 		String sessionId = request.getSession().getId();
+		
+		Subscription sub = new Subscription();
+
 		try {
 			if (request.getParameter(Constants.DELETE_ACTION) != null){
 				lookUpManager.remove(baseObject.getId());
@@ -46,6 +57,14 @@ public class SubscriptionFormController extends TuxBaseFormController {
 						((Subscription)baseObject).getValidation()).booleanValue()){
 					
 					lookUpManager.save((TuxBaseObject)command);
+					
+					User user = userManager.getUserByUsername(request.getRemoteUser());
+					if (request.getRemoteUser() != null && sub.getEmail() != null && sub.getPhoneNumber() != null){
+						sub.setUser(user);
+						sub.setEmail(user.getEmail());	
+						sub.setPhoneNumber(user.getPhoneNumber());
+						((Subscription)baseObject).setEmail(userManager.getUserByUsername(request.getRemoteUser()).getEmail());
+					}					
 					String key = (isNew) ? className+ ".added" : className + ".updated";
 					saveMessage(request, getText(key, locale));
 					return showNewForm(request, response);
@@ -61,4 +80,5 @@ public class SubscriptionFormController extends TuxBaseFormController {
 		}
 		return showForm(request, response, error);
 	}
+	
 }
--- branch/1.0/src/main/webapp/WEB-INF/pages/subscriptionform.jsp
+++ branch/1.0/src/main/webapp/WEB-INF/pages/subscriptionform.jsp
@@ -1,46 +1,52 @@
 <%@ include file="/common/taglibs.jsp" %>
+<h1><fmt:message key="subscribeUsers.message"/></h1>
 
 <head>
-    <title><fmt:message key="subscribeUsers.title"/></title>
+    <title><fmt:message key="subscribeUsers.message"/></title>
     <meta name="heading" content="<fmt:message key='subscribeUsers.heading'/>"/>
-    <meta name="menu" content="AdminMenu"/>
 </head>
+
 <body id="subscribeUsers"/>
-
-<p><fmt:message key="subscribeUsers.message"/></p>
-
 
 <div class="separator"></div>
 
 <input type="button" onclick="location.href='mainMenu.html'" value="<fmt:message key="button.done"/>"/>
 
+
+<c:if test="${hasPermission }">
 <display:table id="list" name="subscription" class="table list" >
 		<display:column property="email" titleKey="subscription.email"/>
-		<display:column property="phone_number" titleKey="subscription.phone_number"/>
 </display:table>
-
+</c:if>
 
 <form:form commandName="subscription" method="post" action="subscriptionform.html" onsubmit="return onFormSubmit(this)" id="subscriptionForm">
-	<form:hidden path="id"/>
+    <form:hidden path="id"/>
+    <form:hidden path="user"/>
+    
 	<ul>
 		<li>
 			<img src="<c:url value="/captcha.html" />" />
 		</li>
+		
+		<li>
+			<appfuse:label key="subscription.phoneNumber" styleClass="desc" />
+			<form:errors path="phoneNumber" cssClass="fieldError"  />
+			<form:input path="phoneNumber"/>
+		</li>
+		
+		<li>
+			<appfuse:label key="subscription.email" styleClass="desc" />
+			<form:errors path="email" cssClass="fieldError"  />
+			<form:input path="email"/>
+		</li>
+	
+
 		<li>
 			<appfuse:label key="subscription.validation" styleClass="desc" />
 			<form:errors path="validation" cssClass="fieldError"  />
 			<form:input path="validation"/>
 		</li>
-		<li>
-			 <appfuse:label styleClass="desc" key="subscription.phone_number"/>
-            <form:errors path="phone_number" cssClass="fieldError"/>
-            <form:input path="phone_number" id="email" cssClass="text medium" cssErrorClass="text medium error"/>
-        </li>  		
-        <li>
-            <appfuse:label styleClass="desc" key="subscription.email"/>
-            <form:errors path="email" cssClass="fieldError"/>
-            <form:input path="email" id="email" cssClass="text medium" cssErrorClass="text medium error"/>
-        </li>  
+	
           
 				
 		<li class="buttonBar bottom">
